0a0aecf4194859f7eea2832fed8bf8aa88814a6a
MugiD <bekslambek22@gmail.com>
2024-09-20 16:58:40 +0500

    feat: add macbook

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,10 +2,11 @@ import Login from "./login"; // Adjust the import path as necessary
 import Link from "next/link";
 
 const Navbar = () => {
-
   return (
     <nav className="flex justify-between items-center px-16 py-4">
-        <Link href="/"className="font-bold">Hack2</Link>
+      <Link href="/" className="font-bold">
+        Hack2
+      </Link>
       <Login />
     </nav>
   );
