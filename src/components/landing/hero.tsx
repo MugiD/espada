cb63b0aff3afc69af834b732fba943174086ff53
MugiD <bekslambek22@gmail.com>
2024-09-20 16:32:18 +0500

    feat: change name

--- src/components/landing/hero.tsx
+++ src/components/landing/hero.tsx
@@ -4,7 +4,7 @@ export default function Hero()  {
     return (
         <section className="py-20 px-16">
         <div className="container mx-auto text-center">
-          <h1 className="text-5xl font-bold mb-4">Participation made easier</h1>
+          <h1 className="text-5xl font-bold mb-4">2nd Generation Hackathons</h1>
           <Button size="lg" variant="outline">Get Started</Button>
         </div>
       </section>
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -5,7 +5,7 @@ const Navbar = () => {
 
   return (
     <nav className="flex justify-between items-center px-16 py-4">
-        <Link href="/"className="font-bold">HackTuah</Link>
+        <Link href="/"className="font-bold">Hack2</Link>
       <Login />
     </nav>
   );
